ec851136681d1c7660e740ce892bb6ec94fe43cf
AndrisBorbas <andris.borbas@gmail.com>
2020-10-18 02:02:35 +0200

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import "../styles.css";
+
+import type { AppProps } from "next/app";
+
+export default function MyApp({
+	Component,
+	pageProps,
+}: AppProps): React.ReactElement | null {
+	return <Component {...pageProps} />;
+}
