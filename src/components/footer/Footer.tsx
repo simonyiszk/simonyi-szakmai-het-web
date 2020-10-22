7de91fb5c1689116658cd8226bbd0353fb7f75a4
Andris Borbas <andris.borbas@gmail.com>
2020-10-22 20:14:34 +0200

    Add vik logo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,16 +3,12 @@ export function Footer(): JSX.Element {
 		<footer className="w-full bg-primary">
 			<div className="container flex flex-1 p-4 justify-between mx-auto flex-col md:flex-row">
 				<a
-					href="https://sch.bme.hu"
-					className="mx-auto p-4"
+					href="https://vik.bme.hu"
+					className="mx-auto p-4 py-2"
 					target="_blank"
 					rel="noreferrer"
 				>
-					<img
-						src="static/logos/sch.svg"
-						alt="Schönherz logo"
-						className="h-12"
-					/>
+					<img src="static/logos/vik.svg" alt="VIK logo" className="h-16" />
 				</a>
 				<a
 					href="https://simonyi.bme.hu"
