1c0516bb9c27a7f1234ab3dcade704bad2b714fa
Andris Borbas <andris.borbas@gmail.com>
2020-10-22 20:10:20 +0200

    Add favicon

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,18 +3,6 @@ export function Footer(): JSX.Element {
 		<footer className="w-full bg-primary">
 			<div className="container flex flex-1 p-4 justify-between mx-auto flex-col md:flex-row">
 				<a
-					href="https://simonyi.bme.hu"
-					className="mx-auto p-4"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<img
-						src="static/logos/simonyi.svg"
-						alt="Simonyi logo"
-						className="h-12"
-					/>
-				</a>
-				<a
 					href="https://sch.bme.hu"
 					className="mx-auto p-4"
 					target="_blank"
@@ -23,6 +11,18 @@ export function Footer(): JSX.Element {
 					<img
 						src="static/logos/sch.svg"
 						alt="Schönherz logo"
+						className="h-12"
+					/>
+				</a>
+				<a
+					href="https://simonyi.bme.hu"
+					className="mx-auto p-4"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<img
+						src="static/logos/simonyi.svg"
+						alt="Simonyi logo"
 						className="h-12"
 					/>
 				</a>
