d502cddb656f6d129efba1b64fbfc8455864cfc3
Freyb <balazs.frey@gmail.com>
2020-11-05 21:27:52 +0100

    Add next-images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
 
 const basePath = process.env.NODE_ENV === "development" ? "" : "/szakmaihet";
 
-module.exports = {
+module.exports = withImages({
 	basePath,
-};
+	trailingSlash: true,
+	assetPrefix: basePath,
+});
 
 /*
 module.exports = withPlugins([
